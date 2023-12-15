8ca9e8f8cf05f658043cd2010e5bbc64c74ad312
Seonjin <128454779+Limseonjin@users.noreply.github.com>
2023-12-15 17:55:22 +0900

    #학생관리시스템 - 삭제모달 및 정보수정 모달 생성

--- test_HTML/main.js
+++ test_HTML/main.js
@@ -1,8 +1,23 @@
-const detail_btn = document.querySelectorAll('.detail_btn');
+const $detail_btn = [...document.querySelectorAll('.detail_btn')];
+const $del_btn = document.querySelector('.delete');
+const $detail_Info = document.querySelector('.student_Detail_Info');
 
-detail_btn.forEach((btn) => {
+$detail_btn.forEach((btn) => {
   btn.addEventListener('click', () => {
-    const $detail_Info = document.querySelector('.student_Detail_Info');
     $detail_Info.classList.remove('non');
+  });
+});
+
+$del_btn.addEventListener('click', () => {
+  const $del_Modal = document.querySelector('.delete_Modal');
+  const $modal_back = document.getElementById('modal_back');
+  const $del_yes = document.getElementById('del_yes');
+  $del_Modal.classList.remove('non');
+  $modal_back.classList.remove('non');
+
+  $del_yes.addEventListener('click', () => {
+    $del_Modal.classList.add('non');
+    $modal_back.classList.add('non');
+    $detail_Info.classList.add('non');
   });
 });
